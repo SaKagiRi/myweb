23e01119d727e73d65adfafdc627e41ab65db8d4
knakto <kasichonooo@gmail.com>
2025-01-30 04:21:22 +0700

    update picture

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/myweb/' : '',
+  basePath: isProd ? '/myweb' : '',
+  output: 'export'
 };
 
 export default nextConfig;
