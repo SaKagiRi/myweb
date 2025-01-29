4f1236bc3f0044aaca1421180b2be9cf335bfb05
knakto <kasichonooo@gmail.com>
2025-01-30 02:38:43 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    webpack(config) {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxSize: 2000000, // 2MB max chunk size
+    };
+    return config;
+  },
+    output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+//export default nextConfig;
