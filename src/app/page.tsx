d1747543f2264667d12db79fd968f2fccae50a39
knakto <kasichonooo@gmail.com>
2025-01-30 22:11:25 +0700

    update to kankto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FaDiscord, FaFacebookSquare, FaYoutube, FaCss3Alt, FaHtml5 } from "react-icons/fa";
 import { SiNixos, SiNextdotjs, SiCoursera } from "react-icons/si";
 import { AiFillInstagram } from "react-icons/ai";
@@ -17,14 +18,14 @@ export default function Home() {
 					<p className="text-2xl font-medium">"</p>
 				</div>
 				<div className="flex flex-row gap-5 w-1/3 justify-center">
-					<a href="#" className="text-2xl font-semibold border-b-2 text-[#33ECD5]">Home</a>
-					<a href="#" className="text-2xl hover:text-[#33ECD5] hover:font-semibold">Blogs</a>
+					<Link href="#" className="text-2xl font-semibold border-b-2 text-[#33ECD5]">Home</Link>
+					<Link href="#" className="text-2xl hover:text-[#33ECD5] hover:font-semibold">Blogs</Link>
 				</div>
 				<div className="text-xl gap-6 flex flex-row mr-10 w-1/3 justify-end">
-					<FaDiscord className="mt-1 text-[#33ECD5]" /><a href="https://discord.gg/UdxFKNXykR" className="hover:font-semibold hover:text-[#33ECD5]">Discord</a>
-					<FaFacebookSquare className="mt-1 text-[#33ECD5]" /><a href="https://web.facebook.com/kasichon?locale=th_TH" className="hover:font-semibold hover:text-[#33ECD5]">Facebook</a>
-					<AiFillInstagram className="mt-1 text-[#33ECD5]" /><a href="https://www.instagram.com/sakagi_ri/" className="hover:font-semibold hover:text-[#33ECD5]">Instagram</a>
-					<FaYoutube className="mt-1 text-[#33ECD5]" /><a href="https://www.youtube.com/@sakagiri_ch.9482" className="hover:font-semibold hover:text-[#33ECD5]">Youtube</a>
+					<FaDiscord className="mt-1 text-[#33ECD5]" /><Link href="https://discord.gg/UdxFKNXykR" className="hover:font-semibold hover:text-[#33ECD5]">Discord</Link>
+					<FaFacebookSquare className="mt-1 text-[#33ECD5]" /><Link href="https://web.facebook.com/kasichon?locale=th_TH" className="hover:font-semibold hover:text-[#33ECD5]">Facebook</Link>
+					<AiFillInstagram className="mt-1 text-[#33ECD5]" /><Link href="https://www.instagram.com/sakagi_ri/" className="hover:font-semibold hover:text-[#33ECD5]">Instagram</Link>
+					<FaYoutube className="mt-1 text-[#33ECD5]" /><Link href="https://www.youtube.com/@sakagiri_ch.9482" className="hover:font-semibold hover:text-[#33ECD5]">Youtube</Link>
 				</div>
 			</nav>
 			<div className="flex justify-center">
@@ -56,7 +57,7 @@ export default function Home() {
 						</div>
 						<div className="flex flex-col items-start">
 							<p className="text-2xl">-- If you're searching for a highly capable backend developer with a passion for learning new things, look no further. --</p>
-							<a href="https://github.com/SaKagiRi" className="bg-[#33ECD5] opacity-80 hover:opacity-100 text-black flex justify-center text-2xl font-semibold rounded-[20px] px-5 py-2 mt-10">Lets see my work.</a>
+							<Link href="https://github.com/SaKagiRi" className="bg-[#33ECD5] opacity-80 hover:opacity-100 text-black flex justify-center text-2xl font-semibold rounded-[20px] px-5 py-2 mt-10">Lets see my work.</Link>
 						</div>
 					</div>
 				</div>
@@ -97,9 +98,9 @@ export default function Home() {
 							<p className="text-2xl/10">I am a Computer Science major at Ramkhamhaeng University and a cadet at 42 Network. My passion lies in controlling everything around me—it feels like true freedom, and I find it absolutely fascinating. My dream is to build a fully automated smart home, where I can control everything exactly the way I want. This passion is what led me to study Computer Science and dive deep into technology.</p>
 							<div className="flex flex-row gap-2 mt-40">
 								<p className="text-xl text-nowrap">Please save my</p>
-								<a href="https://linktr.ee/knxkto?utm_source=linktree_profile_share&ltsid=38f961a0-3c26-4451-b71a-8e4ff75acbb3" className="text-xl text-[#33ECD5] font-semibold border-b-2">Contacts</a>
+								<Link href="https://linktr.ee/knxkto?utm_source=linktree_profile_share&ltsid=38f961a0-3c26-4451-b71a-8e4ff75acbb3" className="text-xl text-[#33ECD5] font-semibold border-b-2">Contacts</Link>
 								<p className="text-xl text-nowrap">or join</p>
-								<a href="https://discord.gg/UdxFKNXykR" className="text-xl text-[#33ECD5] font-semibold border-b-2">Discord</a>
+								<Link href="https://discord.gg/UdxFKNXykR" className="text-xl text-[#33ECD5] font-semibold border-b-2">Discord</Link>
 								<p className="text-xl text-nowrap">because this page is Temporary website, thank you.</p>
 
 							</div>
