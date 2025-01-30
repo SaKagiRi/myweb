ca5131c5d8af95640d4dd962f054fe9e6c9e8a8f
knakto <kasichonooo@gmail.com>
2025-01-30 17:36:17 +0700

    update out

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
+	output: 'export'
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+//export default nextConfig;
